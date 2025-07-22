e7398ff86bacf6ae411b2f1eaa65dac8f5446fbf
bobllor <bobllor@yahoo.com>
2025-07-22 00:36:15 -0500

    new min size for window

--- backend/main.py
+++ backend/main.py
@@ -8,9 +8,10 @@ DB_PATH = f'backend/assets/{DB_NAME}'
 if __name__ == '__main__':
     db: Database = Database(DB_PATH)
     api: API = API(db=db)
+    size: tuple[int, int] = (1280, 720)
 
     title: str = 'TEMPORARY'
     url: str = 'http://localhost:5173/'
 
-    webview.create_window(title, url, js_api=api)
+    webview.create_window(title, url, js_api=api, min_size=size)
     webview.start(debug=True)
