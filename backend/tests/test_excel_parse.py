6ef5b247ba7e04d02460e9be537355b0233de153
teboc <bobllor@yahoo.com>
2025-10-21 00:33:53 -0500

    added tests

---
+++ backend/tests/test_excel_parse.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).parent.parent.absolute()))
+
+from typing import Any
+from core.parser import Parser
+from faker import Faker
+import pandas as pd
+import support.utils as utils
+import random
+
+# this is faked and cleaned data, no sensitive leaks.
+test_json: Path = Path(__file__).parent / "data.json"
+
+FULL_NAME: str = "full name"
+
+def test_read_excel():
+    df: pd.DataFrame = pd.read_json(test_json)
+
+    parser: Parser = Parser(df)
+
+    assert len(parser.get_columns()) > 0
+
+def test_get_excel_data():
+    df: pd.DataFrame = pd.read_json(test_json)
+    parser: Parser = Parser(df)
+
+    assert len(parser.get_rows("full name")) > 0
+
+def test_apply_name():
+    df: pd.DataFrame = pd.read_json(test_json)
+    parser: Parser = Parser(df)
+
+    # adding fake names to the name columns
+    parser.apply(
+        col_name=FULL_NAME, 
+        func=lambda x: x + " " + " ".join([Faker().last_name() for _ in range(0, random.randint(1, 3))])
+    )
+
+    parser.apply(col_name=FULL_NAME, func=utils.validate_name)
+
+    for name in parser.get_rows(FULL_NAME):
+        name: str = name
+        arr: list[str] = name.split()
+
+        if len(arr) != 2:
+            raise AssertionError("Name parsing failed got %s", name)
+    
+    assert True
