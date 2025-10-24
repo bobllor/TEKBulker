4160ddc689f2ccb7850666772ba47bd495ffb1ec
teboc <bobllor@yahoo.com>
2025-10-24 03:38:14 -0500

    added new key for DEFAULT_HEADER_MAP, added comments

--- backend/support/vars.py
+++ backend/support/vars.py
@@ -2,6 +2,9 @@ from pathlib import Path
 from .types import HeaderMap, OpcoMap, TemplateMap, SettingsMap, AzureHeaders
 from typing import Literal
 
+# NOTE: these are default mappings used to initialize the data.
+# the data is based off of ServiceNow naming, but the values can be changed.
+
 AZURE_VERSION: Literal['version:v1.0'] = 'version:v1.0'
 # i dont think order matters for the end csv- it may need testing.
 AZURE_HEADERS: AzureHeaders = {
@@ -13,9 +16,12 @@ AZURE_HEADERS: AzureHeaders = {
     'last_name': 'Last name [surname]',
 }
 
+# NOTE: value is the column name in the excel/csv file.
+# this dict gets reversed when it gets validated in Parser.
 DEFAULT_HEADER_MAP: HeaderMap = {
     'opco': 'operating company',
     'name': 'full name',
+    'country': 'country/territory',
 }
 
 # no @ is used here because it is added in to the username generator
