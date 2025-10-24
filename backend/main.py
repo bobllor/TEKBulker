9559f6f33205bf508c5ba37ddaffb56c024f621f
teboc <bobllor@yahoo.com>
2025-10-24 03:38:39 -0500

    updated API logic

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,29 @@
 from core.json_reader import Reader
 from api.api import API
 from logger import Log
-from support.vars import DEFAULT_HEADER_MAP, DEFAULT_SETTINGS_MAP
+from support.vars import DEFAULT_HEADER_MAP, DEFAULT_OPCO_MAP, DEFAULT_SETTINGS_MAP
 import webview
 
-MAP_FILE: str = 'data-mapping.json'
+EXCEL_FILE: str = 'EXCEL-mapping.json'
 SETTINGS_FILE: str = 'settings.json'
-MAP_PATH: str = f'config/{MAP_FILE}'
+OPCO_FILE: str = "opco-mapping.json"
+EXCEL_PATH: str = f'config/{EXCEL_FILE}'
 SETTINGS_PATH: str = f'config/{SETTINGS_FILE}'
+OPCO_PATH: str = f"config/{OPCO_FILE}"
 
 if __name__ == '__main__':
     logger: Log = Log()
 
-    mapping_reader: Reader = Reader(MAP_FILE, defaults=DEFAULT_HEADER_MAP, logger=logger)
+    excel_reader: Reader = Reader(EXCEL_FILE, defaults=DEFAULT_HEADER_MAP, logger=logger)
     settings_reader: Reader = Reader(SETTINGS_PATH, defaults=DEFAULT_SETTINGS_MAP, logger=logger)
-    api: API = API(settings_reader=settings_reader, mapping_reader=mapping_reader, logger=logger)
+    opco_reader: Reader = Reader(OPCO_PATH, defaults=DEFAULT_OPCO_MAP, logger=logger)
+
+    api: API = API(
+        settings_reader=settings_reader, 
+        excel_reader=excel_reader,
+        opco_reader=opco_reader,
+        logger=logger
+    )
     size: tuple[int, int] = (1280, 720)
 
     title: str = 'TEMPORARY'
