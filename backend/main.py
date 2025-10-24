5a454a610885b366f835b4d17095586211d92b7e
teboc <bobllor@yahoo.com>
2025-10-24 00:42:46 -0500

    updated main for removal of database

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,20 @@
-from db.database import Database
+from core.json_reader import Reader
 from api.api import API
 from logger import Log
+from support.vars import DEFAULT_HEADER_MAP, DEFAULT_SETTINGS_MAP
 import webview
 
-DB_NAME = 'azuregenerator.sqlite'
-DB_PATH = f'backend/assets/{DB_NAME}'
+MAP_FILE: str = 'data-mapping.json'
+SETTINGS_FILE: str = 'settings.json'
+MAP_PATH: str = f'config/{MAP_FILE}'
+SETTINGS_PATH: str = f'config/{SETTINGS_FILE}'
 
 if __name__ == '__main__':
     logger: Log = Log()
 
-    db: Database = Database(DB_PATH, logger=logger)
-    api: API = API(db, logger=logger)
+    mapping_reader: Reader = Reader(MAP_FILE, defaults=DEFAULT_HEADER_MAP, logger=logger)
+    settings_reader: Reader = Reader(SETTINGS_PATH, defaults=DEFAULT_SETTINGS_MAP, logger=logger)
+    api: API = API(settings_reader=settings_reader, mapping_reader=mapping_reader, logger=logger)
     size: tuple[int, int] = (1280, 720)
 
     title: str = 'TEMPORARY'
