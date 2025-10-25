13a32c672e46d945640f6642c4140d0100754c5f
teboc <bobllor@yahoo.com>
2025-10-25 04:16:43 -0500

    untracked files on main: 02866ae Merge pull request #1 from bobllor/dev

---
+++ backend/tests/fixtures.py
@@ -0,0 +1,49 @@
+from logger import Log
+from pathlib import Path
+from core.json_reader import Reader
+from api.api import API
+from support.vars import DEFAULT_HEADER_MAP, DEFAULT_SETTINGS_MAP, DEFAULT_OPCO_MAP
+import pandas as pd
+import pytest
+
+JSON: Path = Path(__file__).parent / "data.json"
+
+@pytest.fixture
+def logger():
+    logger: Log = Log()
+
+    yield logger
+
+@pytest.fixture
+def reader(tmp_path: Path):
+    json_name: str = "temp_reader.json"
+    json_path: Path = tmp_path / "cfg" / json_name
+
+    yield Reader(json_path, defaults=DEFAULT_HEADER_MAP, is_test=True)
+
+@pytest.fixture
+def api(tmp_path: Path):
+    config_path: Path = tmp_path / "config"
+
+    excel: Reader = Reader(config_path / "excel.json", defaults=DEFAULT_HEADER_MAP, is_test=True)
+    settings: Reader = Reader(config_path / "settings.json", defaults=DEFAULT_SETTINGS_MAP, is_test=True)
+    opcos: Reader = Reader(config_path / "opcos.json", defaults=DEFAULT_OPCO_MAP, is_test=True)
+
+    opco_map: dict[str, str] = {
+        "default": DEFAULT_OPCO_MAP["default"],
+        "company one": "company.one.org",
+        "company two": "companytwo.com",
+        "company three": "company.three.nhs.gov"
+    }
+
+    opcos.insert_many(opco_map)
+
+    api: API = API(excel_reader=excel, settings_reader=settings, opco_reader=opcos)
+
+    yield api
+
+@pytest.fixture
+def df():
+    df: pd.DataFrame = pd.read_json(JSON)
+
+    yield df
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,48 @@
+import { JSX, useEffect } from "react";
+import { NavigateFunction } from "react-router";
+import { useNavigate } from "react-router";
+import { useModalContext } from "../context/ModalContext";
+import { FormStateProps } from "./FormComponents/manualUtils/types";
+
+const buttons: Array<{label: string, url: string}> = [
+    {label: 'Home', url: '/'},
+    {label: 'Custom', url: '/custom'}
+]
+
+export default function Navigation({formState}: {formState: FormStateProps}
+    ): JSX.Element{
+    let navigate: NavigateFunction = useNavigate();
+
+    const { revealModal } = useModalContext();
+
+    async function clickWrapper(url: string): Promise<void>{
+      // FIXME: when its ready to be packaged into an app this has to be changed.
+      if(window.location.pathname == url) return;
+
+      // only used if a form has been edited before navigation
+      if(formState.state){
+        const action: boolean = await revealModal('Changes you made are not saved. Are you sure you want to leave?');
+
+        if(!action){
+          return;
+        }
+
+        formState.func(false);
+      }
+
+      navigate(url);
+    }
+    
+    return (
+        <>  
+            {buttons.map((obj, i) => (
+              <div key={i}
+              onClick={() => clickWrapper(obj.url)}
+              className={`py-1 px-2 w-20 flex justify-center items-center rounded-xl bg-gray-300
+              ${obj.url == window.location.pathname && "bg-gray-400"}`}>
+                {obj.label}
+              </div>
+            ))}
+        </>
+    )
+}
