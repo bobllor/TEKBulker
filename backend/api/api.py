9f3732db786a572c642b6ab9158bb74e7f2a59f2
teboc <bobllor@yahoo.com>
2025-10-25 20:45:17 -0500

    updated csv generation logic, new reader val method, and doc changes

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,11 +1,12 @@
 from core.json_reader import Reader
 from core.parser import Parser
 from core.azure_writer import AzureWriter
-from support.types import GenerateCSVProps
+from support.types import GenerateCSVProps, ManualCSVProps
 from base64 import b64decode
 from io import BytesIO
 from logger import Log
 from pathlib import Path
+from typing import Any, Literal
 from support.vars import DEFAULT_HEADER_MAP, DEFAULT_OPCO_MAP, DEFAULT_SETTINGS_MAP
 import support.utils as utils
 import pandas as pd
@@ -15,6 +16,22 @@ class API:
             excel_reader: Reader, settings_reader: Reader,
             opco_reader: Reader, logger: Log = None
         ):
+        '''API class.
+        
+        Parameters
+        ----------
+            excel_reader: Reader
+                The Reader used for the Excel columns for reading and parsing files.
+            
+            settings_reader: Reader
+                The Reader used for handling program settings.
+
+            opco_reader: Reader
+                The Reader used for handling operating company-domain name key-value mapping.
+            
+            logger: Log, default None
+                The logger, if None is given then it will be a default logger.
+        '''
         self.excel: Reader = excel_reader
         self.settings: Reader = settings_reader
         self.opco: Reader = opco_reader
@@ -69,17 +86,20 @@ class API:
 
         # the mapping of the operating company to their domain name.
         opco_mappings: dict[str, str] = self.opco.get_content()
+        usernames: list[str] = utils.generate_usernames(names, opcos, opco_mappings)
 
         writer: AzureWriter = AzureWriter(logger=self.logger)
 
         writer.set_full_names(names)
         writer.set_names(names)
         writer.set_block_sign_in(len(names), []) 
-        writer.set_usernames(names, opcos=opcos, opco_map=opco_mappings)
+        writer.set_usernames(usernames)
         writer.set_passwords([utils.generate_password(20) for _ in range(len(names))])
 
         csv_name: str = f"{utils.get_date()}-az-bulk.csv"
-        writer.write(Path(self.get_output_dir()) / csv_name)
+        writer.write(Path(self.get_reader_value("output_dir", "settings")) / csv_name)
+
+        self.logger.info(f"Generated {csv_name} at {self.get_reader_value('output_dir', 'settings')}")
 
         return utils.generate_response(
             status='success', 
@@ -87,18 +107,27 @@ class API:
             status_code=200
         )
     
-    def generate_manual_csv(self, content: list[dict[str, str]]) -> dict[str, str]:
-        '''Generates the Azure CSV file for bulk accounts through the manual input.'''
+    def generate_manual_csv(self, content: list[ManualCSVProps]) -> dict[str, str]:
+        '''Generates the Azure CSV file for bulk accounts through the manual input.
+        
+        Parameters
+        ----------
+            content: list[ManualCSVProps]
+                A list of dictionaries to convert into a DataFrame for a CSV.
+                Each dictionary represents a row to be added.
+        '''
         names: list[str] = []
         opcos: list[str] = []
+        usernames: list[str] = []
 
         seen_names: dict[str, int] = {}
+        opco_mappings: dict[str, str] = self.opco.get_content()
 
         # contains name, opco, and id. id is not relevant to this however.
         # i could also possibly add in the block sign in values in the content...
         for obj in content:
-            name: str = utils.format_name(obj['name'])
-            names.append(name)
+            name: str = utils.format_name(obj["name"])
+            opco: str = obj["opco"]
 
             # assume duplicate names are unique, a number is added to distinguish the username.
             if name not in seen_names:
@@ -108,30 +137,43 @@ class API:
 
                 name = name + str(seen_names.get(name))
 
-            opcos.append(obj["opco"])
-        
+            names.append(name)
+            usernames.append(utils.generate_username(name, opco, opco_mappings))
+            opcos.append(opco)
 
         passwords: list[str] = [utils.generate_password() for _ in range(len(names))]
-        # FIXME
-        opco_map: dict[str, str] = self.mapping.get_table_data("opco")
+
         writer: AzureWriter = AzureWriter(logger=self.logger)
 
         writer.set_full_names(names)
-        writer.set_usernames(names, opcos=opcos, opco_map=opco_map)
+        writer.set_usernames(usernames)
         writer.set_passwords(passwords)
         writer.set_block_sign_in(len(names), [])
         writer.set_names(names)
 
         csv_name: str = f"{utils.get_date()}-az-bulk.csv"
-        writer.write(Path(self.get_output_dir()) / csv_name)
+        writer.write(Path(self.get_reader_value("output_dir", "settings")) / csv_name)
 
-        return utils.generate_response(status='success', message='')
+        self.logger.info(f"Manual generated {csv_name} at {self.get_reader_value('output_dir', 'settings')}")
 
-    def get_output_dir(self) -> str:
-        '''Retrieve the output directory.'''
-        key: str = 'output_dir'
+        return utils.generate_response(status='success', message='', status_code=200)
+    
+    def get_reader_value(self, key: str, reader: Literal["settings", "opco", "excel"]) -> Any:
+        '''Gets the values from any Reader keys. If the key does not exist,
+        then an empty string is returned.'''
+        readers: dict[str, Reader] = {
+            "settings": self.settings,
+            "opco": self.opco,
+            "excel": self.excel,
+        }
 
-        return self.settings.get(key)
+        val: Any = readers[reader].get(key)
+
+        if val is None:
+            self.logger.error(f"Key {key} does not exist in {reader}")
+            return ""
+        
+        return val
     
     def set_output_dir(self, dir_: Path | str = None) -> dict[str, str]:
         '''Update the output directory.'''
