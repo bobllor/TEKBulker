7fe3a3e63bbc1e4286f096ba89bfe9c81273e2b8
teboc <bobllor@yahoo.com>
2025-11-02 01:03:10 -0600

    removed component dismissing from settings, created useDismissRoute and added to BackgroundBlur

--- src/components/ui/BackgroundBlur.tsx
+++ src/components/ui/BackgroundBlur.tsx
@@ -1,9 +1,15 @@
-import { JSX } from "react";
+import React, { JSX, useRef } from "react";
+import { useDismissRoute } from "../../hookUtils";
 
-export default function BackgroundBlur(): JSX.Element{
+export default function BackgroundBlur({setComponentState}: {setComponentState: React.Dispatch<React.SetStateAction<boolean>>}): JSX.Element{
+    const bgDivRef = useRef<HTMLDivElement>(null);
+
+    useDismissRoute(bgDivRef, setComponentState);
+
     return (
         <>
-            <div className="bg-gray-300/60 blur-lg absolute w-full h-full" />
+            <div ref={bgDivRef}
+            className="bg-gray-300/40 backdrop-blur-2xl absolute w-full h-full z-3" />
         </>
     )    
 }
--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,54 +1,18 @@
-import React, { JSX, useEffect, useRef } from "react";
-import { useLocation, useNavigate } from "react-router";
+import React, { JSX } from "react";
 import BackgroundBlur from "../components/ui/BackgroundBlur";
 import Navigation from "../components/SettingsComponents/Navigation";
 import General from "../components/SettingsComponents/General";
-import { BORDER_COLOR, OUTER_BORDER_THICKNESS, OUTER_MODAL_ROUNDING } from "../components/SettingsComponents/vars";
 
 export default function Settings({setShowSetting}: 
         {setShowSetting: React.Dispatch<React.SetStateAction<boolean>>}): JSX.Element{
-    const location = useLocation();
-    const navigate = useNavigate();
-    const settingsRef = useRef(null);
-
-    useEffect(() => {
-        const closeSettings = () => {
-            navigate(location.state?.previousLocation || "/");
-            setShowSetting(false);
-        }
-        const onClick = (e: MouseEvent) => {
-            if(e.target != settingsRef.current){
-                closeSettings();
-            }
-        }
-
-        const onKeyDown = (e: KeyboardEvent) => {
-            switch(e.key){
-                case "Escape":
-                    closeSettings();
-                default:
-                    return;
-            }
-        }
-
-        document.addEventListener("click", onClick);
-        document.addEventListener("keydown", onKeyDown);
-
-        return () => {
-            document.removeEventListener("click", onClick);
-        }
-    }, [])
-
     return (
         <>
             <div
-            ref={settingsRef}
-            className={`absolute w-150 h-100 z-3 bg-white ${OUTER_MODAL_ROUNDING}
-            ${OUTER_BORDER_THICKNESS} border-${BORDER_COLOR}`}> 
+            className={`absolute w-150 h-100 z-4 bg-white settings-modal-border`}> 
                 <Navigation />
                 <General />
             </div> 
-            <BackgroundBlur />
+            <BackgroundBlur setComponentState={setShowSetting} />
         </>
     )
 }
