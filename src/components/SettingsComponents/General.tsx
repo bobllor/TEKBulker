1afbfc4022133409a7ce142cb3a50d12b6112d0d
teboc <bobllor@yahoo.com>
2025-11-02 01:03:33 -0600

    updated panel components, removed vars

--- src/components/SettingsComponents/General.tsx
+++ src/components/SettingsComponents/General.tsx
@@ -1,11 +1,10 @@
 import { JSX } from "react";
-import { OUTER_MODAL_ROUNDING } from "./vars";
 
 export default function General(): JSX.Element{
     return (
         <>
             <div className={`h-full w-[65%] bg-gray-200 absolute right-0
-            ${OUTER_MODAL_ROUNDING}`}>
+                settings-right-panel`}>
 
             </div>
         </>
--- src/components/SettingsComponents/Navigation.tsx
+++ src/components/SettingsComponents/Navigation.tsx
@@ -1,14 +1,29 @@
-import { JSX } from "react";
-import { RIGHT_BORDER_THICKNESS, TL_MODAL_ROUNDING, BL_MODAL_ROUNDING } from "./vars";
+import { JSX, useState } from "react";
+
+const buttons: Array<SettingsButton> = [
+    {label: "General"}
+]
 
 export default function Navigation(): JSX.Element{
+    // by default it will always be general
+    const [selectedSetting, setSelectedSetting] = useState(buttons[0].label);
+    
     return (
         <>
             <div className={`h-full w-[35%] bg-gray-500/60 absolute 
-            ${TL_MODAL_ROUNDING} ${BL_MODAL_ROUNDING}
-            ${RIGHT_BORDER_THICKNESS}`}>
-
+            settings-left-panel p-2`}>
+                {buttons.map((ele) => (
+                    <div className="w-full h-8 rounded-xl hover:bg-gray-300 flex items-center p-3">
+                        <span className="flex items-center text-xl">
+                            {ele.label}
+                        </span>
+                    </div>
+                ))}
             </div>
         </>
     )
+}
+
+type SettingsButton = {
+    label: string,
 }
