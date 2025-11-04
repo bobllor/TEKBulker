a1b9ac873318999cad4bea8261c432500f722e38
teboc <bobllor@yahoo.com>
2025-11-03 21:34:37 -0600

    added slider button component for future work

---
+++ src/components/ui/SliderButton.tsx
@@ -0,0 +1,11 @@
+import { JSX } from "react";
+
+export default function SliderButton(): JSX.Element{
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    )
+}
