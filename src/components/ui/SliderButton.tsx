202515e697919de99f94cee9e424cfd4d5975d6a
teboc <bobllor@yahoo.com>
2025-11-09 06:29:04 -0600

    updated sliderbutton, added loading for future use

--- src/components/ui/SliderButton.tsx
+++ src/components/ui/SliderButton.tsx
@@ -1,15 +1,23 @@
 import React, { JSX, useState } from "react";
 import { SliderButtonProps } from "./types";
 
-export default function SliderButton({func}: {func: (...args: any[]) => any}): JSX.Element{
-    const [active, setActive] = useState(false);
+/**
+ * Creates a new SliderButton. Contains a `status` boolean state
+ * which can be used with `func`. The boolean argument should be last in `func`.
+ * @param func - Any function. SliderButton has a `status` boolean state and the 
+ * function can use this if `useStatus` is true.
+ * @param useStatus - Indicates that the `status` state should be passed with `func`.
+ * @returns jsxElement
+ */
+export default function SliderButton({func, useStatus = false}: {func: (...args: any[]) => any, useStatus?: boolean}): JSX.Element{
+    const [status, setStatus] = useState(false);
 
     return (
         <>
             <div
-            onClick={() => onClick(active, setActive, func)}
+            onClick={() => onClick(status, setStatus, func)}
             className={`flex rounded-3xl items-center w-10 h-6 relative px-0.5
-                ${!active ? "bg-white justify-start" : "bg-blue-500 justify-end"}`}>
+                ${!status ? "bg-white justify-start" : "bg-blue-500 justify-end"}`}>
                 <div 
                 className="rounded-2xl bg-blue-300 w-5 h-5 "/>
             </div>
