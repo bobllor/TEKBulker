d266b9edfd622b0ece52a5e63c774869a9ead3e7
teboc <bobllor@yahoo.com>
2025-11-11 00:58:52 -0600

    added generateID for utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import "./pywebview";
+
 /**
  * Checks if the values of two objects are the same or different. 
  * @param obj - The base object.
@@ -22,4 +24,24 @@ export function compareObjects(baseObj: Object, objToCompare: Object): boolean{
     }
 
     return hasDifferentValue;
+}
+
+/**
+ * Generates a random ID.
+ * @returns {string} The ID.
+ */
+export function generateId(): string{
+    const id: string = Math.random().toString(16).slice(2);
+
+    return id;
+}
+
+/**
+ * Checks if the response of the backend call is successful. Requires the response to
+ * have the key `status`.
+ * @param res - The response object, map, or record.
+ * @returns {boolean} - If the response is not successful.
+ */
+export function checkRes(res: Record<string, string>): boolean{
+    return res["status"] == "success";
 }
