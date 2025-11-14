ae9c7fba967c3a946c4a21a2af2f7728122d5aa4
teboc <bobllor@yahoo.com>
2025-11-13 22:49:39 -0600

    updated ui components

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,24 +1,42 @@
 import { JSX } from "react";
-import { ButtonProps } from "./types";
 
 /**
- * A submit button component. 
+ * A button component. 
  * @param text The text of the button 
  * @param bg Tailwind class string for the background of the button, by default it is bg-blue-500
  * @param bgHover Tailwind class string for the hover background of the button, by default it is bg-blue-400 
  * @param paddingX Number representing the padding amount for the X-axis
  * @param paddingY Number representing the padding amount for the Y-axis
+ * @param type The button type
+ * @param func The function used when the button is clicked, by default it is `undefined`
+ * @param triggerSettingsUpdate Used to trigger to update the settings for the program, by default it is `false`
  */
 export default function Button(
     {text, bg = "bg-blue-500", bgHover = "bg-blue-400", 
-    paddingX, paddingY}: ButtonProps): JSX.Element{
+    paddingX = 2, paddingY = 2, type = "submit", func = undefined}: ButtonProps): JSX.Element{
+
     return (
         <>
             <button
+            onClick={() => {
+                if(func){
+                    func();
+                }
+            }}
             className={`px-${paddingX} py-${paddingY} rounded-xl ${bg} text-white hover:${bgHover}`}
-            type="submit">
+            type={type}>
                 {text}
             </button>
         </>
     )
+}
+
+type ButtonProps = {
+    text: string,
+    bg?: string,
+    bgHover?: string,
+    paddingX?: number,
+    paddingY?: number,
+    func?: () => any | undefined,
+    type?: "submit" | "reset" | "button" | undefined,
 }
--- src/components/ui/SliderButton.tsx
+++ src/components/ui/SliderButton.tsx
@@ -1,36 +1,25 @@
-import React, { JSX, useState } from "react";
-import { SliderButtonProps } from "./types";
+import React, { JSX, useEffect, useState } from "react";
 
 /**
  * Creates a new SliderButton. Contains a `status` boolean state
  * which can be used with `func`. The boolean argument should be last in `func`.
- * @param func - Any function. SliderButton has a `status` boolean state and the 
- * function can use this if `useStatus` is true.
- * @param useStatus - Indicates that the `status` state should be passed with `func`.
- * @returns jsxElement
+ * @param func Any function. SliderButton has a `status` boolean state and the 
+ * function can use this if `useStatus` is true
+ * @param initialStatus The initial status of the button
  */
-export default function SliderButton({func, useStatus = false}: {func: (...args: any[]) => any, useStatus?: boolean}): JSX.Element{
-    const [status, setStatus] = useState(false);
+export default function SliderButton({func, initialStatus}: 
+    {func: (...args: any[]) => any, initialStatus: boolean}): JSX.Element{
 
     return (
         <>
             <div
-            onClick={() => onClick(status, setStatus, func)}
+            onClick={() => func(initialStatus)}
             className={`flex rounded-3xl items-center w-10 h-6 relative px-0.5
-                ${!status ? "bg-white justify-start" : "bg-blue-500 justify-end"}
+                ${!initialStatus ? "bg-white justify-start" : "bg-blue-500 justify-end"}
                 transition-all duration-400`}>
                 <div 
                 className="rounded-2xl bg-blue-300 w-5 h-5 "/>
             </div>
         </>
     )
-}
-
-async function onClick(
-    buttonStatus: boolean, 
-    setButtonStatus: React.Dispatch<React.SetStateAction<boolean>>, 
-    func: (...args: any[]) => any){
-        setButtonStatus(prev => !prev);
-
-        func(setButtonStatus);
 }
