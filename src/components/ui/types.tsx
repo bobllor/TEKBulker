3151cfd64f2fdb587e29bfc2b1b9dc1c0c949a56
teboc <bobllor@yahoo.com>
2025-11-13 22:48:41 -0600

    removed unused import

--- src/components/ui/types.tsx
+++ src/components/ui/types.tsx
@@ -1,14 +1,5 @@
 import React from "react"
 
-export type ButtonProps = {
-    text: string,
-    bg?: string,
-    bgHover?: string,
-    paddingX: number,
-    paddingY: number,
-    func?: () => any,
-}
-
 export type SliderButtonProps = {
     status: boolean,
     statusSetter: React.Dispatch<React.SetStateAction<boolean>>,
